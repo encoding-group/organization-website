ba618304b1beb8a840b0454c1807adf294a5b97a
René Weiser <rene.weiser@uni-weimar.de>
2020-07-01 13:21:09 +0200

    Refined some project settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    }
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
